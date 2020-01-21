bb355cae78fca0321e0efbf51b0fcd0aa7f0a081
t822660 <t822660@spark.co.nz>
2020-01-21 15:56:29 +1300

    created

---
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,44 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+
+public class CreateDemo {
+
+	public static void main(String args[]) {
+		
+		SessionFactory factory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.buildSessionFactory();
+		
+		Session session=factory.getCurrentSession();
+				
+		try {
+			
+			session.beginTransaction();
+
+	
+			session.getTransaction().commit();
+			System.out.println("Done!");
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+		
+	}
+	
+}
